b40fc92ffcf6e5ef022f2614288d9c7374a23623
Michael Makulov <mmn95@mail.ru>
2022-01-16 22:55:33 +0500

    -Добавлен класс AppState состояния приложения и классы для получения данных из API Skyeng (DataModel, Meanings, Translation)

---
+++ app/src/main/java/ru/mmn/translatorapp/Meanings.kt
@@ -0,0 +1,10 @@
+package ru.mmn.translatorapp
+
+import com.google.gson.annotations.SerializedName
+
+class Meanings(
+    @field:SerializedName("translation") val translation: Translation?,
+    @field:SerializedName("imageUrl") val imageUrl: String?
+
+    //TODO "transcription"
+)
---
+++ app/src/main/java/ru/mmn/translatorapp/Translation.kt
@@ -0,0 +1,5 @@
+package ru.mmn.translatorapp
+
+import com.google.gson.annotations.SerializedName
+
+class Translation(@field:SerializedName("text") val translation: String?)
