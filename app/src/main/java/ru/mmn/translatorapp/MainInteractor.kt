6456be7f824a471ba583f21234cff6596cfd63c1
Michael Makulov <mmn95@mail.ru>
2022-01-17 20:57:02 +0500

    -Созданы классы имплементации презентера и интерактора

---
+++ app/src/main/java/ru/mmn/translatorapp/MainInteractor.kt
@@ -0,0 +1,18 @@
+package ru.mmn.translatorapp
+
+import io.reactivex.Observable
+
+class MainInteractor(
+
+    private val remoteRepository: Repository<List<DataModel>>,
+    private val localRepository: Repository<List<DataModel>>
+) : Interactor<AppState> {
+
+    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
+        return if (fromRemoteSource) {
+            remoteRepository.getData(word).map { AppState.Success(it) }
+        } else {
+            localRepository.getData(word).map { AppState.Success(it) }
+        }
+    }
+}
