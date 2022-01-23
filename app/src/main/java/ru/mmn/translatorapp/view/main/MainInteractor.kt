8b3baabb9f4684bdd1311d429608afcdad8a5c2d
Michael Makulov <mmn95@mail.ru>
2022-01-23 15:33:47 +0500

    -Внедрение корутин в проект

--- app/src/main/java/ru/mmn/translatorapp/view/main/MainInteractor.kt
+++ app/src/main/java/ru/mmn/translatorapp/view/main/MainInteractor.kt
@@ -1,6 +1,5 @@
 package ru.mmn.translatorapp.view.main
 
-import io.reactivex.Observable
 import ru.mmn.translatorapp.model.data.AppState
 import ru.mmn.translatorapp.model.data.DataModel
 import ru.mmn.translatorapp.model.repository.Repository
@@ -8,15 +7,17 @@ import ru.mmn.translatorapp.viewmodel.Interactor
 
 
 class MainInteractor(
-    private val remoteRepository: Repository<List<DataModel>>,
-    private val localRepository: Repository<List<DataModel>>
+    private val repositoryRemote: Repository<List<DataModel>>,
+    private val repositoryLocal: Repository<List<DataModel>>
 ) : Interactor<AppState> {
 
-    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
-        return if (fromRemoteSource) {
-            remoteRepository
-        } else {
-            localRepository
-        }.getData(word).map {AppState.Success(it)}
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): AppState {
+        return AppState.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
     }
 }
