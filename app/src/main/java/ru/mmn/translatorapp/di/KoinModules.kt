7d6732f143841fc7eac1a55c94e2f9e959e1d5ef
Michael Makulov <mmn95@mail.ru>
2022-01-30 12:41:40 +0500

    -Внедрение и реализация Room database

--- app/src/main/java/ru/mmn/translatorapp/di/KoinModules.kt
+++ app/src/main/java/ru/mmn/translatorapp/di/KoinModules.kt
@@ -1,31 +1,44 @@
 package ru.mmn.translatorapp.di
 
-import org.koin.core.qualifier.named
+import androidx.room.Room
 import org.koin.dsl.module
+import ru.mmn.translatorapp.database.HistoryDataBase
 import ru.mmn.translatorapp.model.data.DataModel
 import ru.mmn.translatorapp.model.datasource.RetrofitImplementation
 import ru.mmn.translatorapp.model.datasource.RoomDataBaseImplementation
 import ru.mmn.translatorapp.model.repository.Repository
 import ru.mmn.translatorapp.model.repository.RepositoryImplementation
+import ru.mmn.translatorapp.view.history.HistoryInteractor
 import ru.mmn.translatorapp.view.main.MainInteractor
-import ru.mmn.translatorapp.view.main.MainViewModel
+import ru.mmn.translatorapp.viewmodel.HistoryViewModel
+import ru.mmn.translatorapp.viewmodel.MainViewModel
 
 
 val application = module {
 
-    single<Repository<List<DataModel>>>(named(NAME_REMOTE)) {
+    single { Room.databaseBuilder(get(), HistoryDataBase::class.java,
+    "HistoryDB").build() }
+
+    single { get<HistoryDataBase>().historyDao() }
+
+    single<Repository<List<DataModel>>> {
         RepositoryImplementation(
             RetrofitImplementation()
         )
     }
-    single<Repository<List<DataModel>>>(named(NAME_LOCAL)) {
+    single<Repository<List<DataModel>>> {
         RepositoryImplementation(
             RoomDataBaseImplementation()
         )
     }
 }
 val mainScreen = module {
-    factory { MainInteractor(get(named(NAME_REMOTE)), get(named(NAME_LOCAL))) }
+    factory { MainInteractor(get(), get()) }
     factory { MainViewModel(get()) }
+}
+
+val historyScreen = module {
+    factory { HistoryViewModel(get()) }
+    factory { HistoryInteractor(get(), get()) }
 }
 
---
+++ app/src/main/java/ru/mmn/translatorapp/model/repository/RepositoryImplementationLocal.kt
@@ -0,0 +1,17 @@
+package ru.mmn.translatorapp.model.repository
+
+import ru.mmn.translatorapp.model.data.AppState
+import ru.mmn.translatorapp.model.data.DataModel
+import ru.mmn.translatorapp.model.datasource.DataSourceLocal
+
+class RepositoryImplementationLocal(private val dataSource: DataSourceLocal<List<DataModel>>) :
+    RepositoryLocal<List<DataModel>> {
+
+    override suspend fun getData(word: String): List<DataModel> {
+        return dataSource.getData(word)
+    }
+
+    override suspend fun saveToDB(appState: AppState) {
+        dataSource.saveToDB(appState)
+    }
+}
